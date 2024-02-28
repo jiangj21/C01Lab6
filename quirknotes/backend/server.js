c753c497665146befbf765df7d27d79c3a4addd9
jenna <iamjinjiang@gmail.com>
2024-02-28 14:16:48 -0500

    lab 6 set up complete

--- quirknotes/backend/server.js
+++ quirknotes/backend/server.js
@@ -4,11 +4,17 @@ import cors from "cors";
 
 const app = express();
 const PORT = 4000;
-const mongoURL = "mongodb://127.0.0.1:27017";
+//const mongoURL = "mongodb://127.0.0.1:27017";
 const dbName = "quirknotes";
 
 // Connect to MongoDB
 let db;
+let mongoURL;
+if(process.env.ENV == 'Docker'){
+  mongoURL = 'mongodb://mongodb:27017';
+} else {
+  mongoURL = 'mongodb://127.0.0.1:27017';
+}
 
 async function connectToMongo() {
   const client = new MongoClient(mongoURL);
